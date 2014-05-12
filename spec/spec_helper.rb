3a9cdade6e7cb2e16f8bcf74d5d4846dd563484e
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-05-12 21:44:35 +0200

    Setup testing basis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'puppet'
 require 'rspec'
 require 'rspec-puppet'
+require 'simplecov'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
